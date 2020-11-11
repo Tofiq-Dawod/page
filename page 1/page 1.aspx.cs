c3d4e530bd31ff0f509862fdc87c150631504bf4
Tofiq-Dawod <https://github.com/Tofiq-Dawod/page.git>
2020-11-11 18:51:33 +0200

    first

---
+++ page 1/page 1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace page_1
+{
+    public partial class page_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            txt2.Text = txt1.Text;
+            txt1.Text = "";
+        }
+    }
+}
